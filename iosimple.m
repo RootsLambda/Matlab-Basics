ec7f17d69d2763c38331a3b96d13d89eaaabc5ac
DongMinhQuoc <98574052+RootsLambda@users.noreply.github.com>
2022-10-02 19:25:38 +0700

    Add files via upload

---
+++ iosimple.m
@@ -0,0 +1,14 @@
+% File iosimple.m
+% Introduction to simple export, import, and annotation commands%
+
+% Input using the function input()
+a=input('Input value a:') % string type is enclosed in single quotes
+
+% Export using disp() function
+disp('Vaule of a:')
+disp(a)
+% Annotating on a line we use the % operator
+
+% If you do not want the results to be displayed on the screen,
+% then after each calculation, we end with a semicolon ;
+a=3;
